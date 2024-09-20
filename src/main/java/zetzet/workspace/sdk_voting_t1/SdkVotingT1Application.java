2aba50b2e0a00a594b86d28b0f11eae7104ab777
zetzet21 <ilyabeskrovny@mail.ru>
2024-09-20 23:00:37 +0400

    init commit

---
+++ src/main/java/zetzet/workspace/sdk_voting_t1/SdkVotingT1Application.java
@@ -0,0 +1,13 @@
+package zetzet.workspace.sdk_voting_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdkVotingT1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdkVotingT1Application.class, args);
+    }
+
+}
---
+++ src/test/java/zetzet/workspace/sdk_voting_t1/SdkVotingT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package zetzet.workspace.sdk_voting_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdkVotingT1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
