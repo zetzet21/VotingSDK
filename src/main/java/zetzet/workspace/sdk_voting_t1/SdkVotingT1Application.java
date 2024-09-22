2c5f11409e0329cac6d4b6637bc0df46e8d93b75
gzevs <grom_zetom@mail.ru>
2024-09-22 18:27:48 +0400

    fix

--- src/main/java/zetzet/workspace/sdk_voting_t1/SdkVotingT1Application.java
+++ src/main/java/zetzet/workspace/sdk_voting_t1/SdkVotingT1Application.java
@@ -8,7 +8,6 @@ public class SdkVotingT1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SdkVotingT1Application.class, args);
-        //new SdkVotingT1Application();
     }
 
 }
